ee6b4ce975ede9c73f3bdd01f987711e3d5a9c52
dasalgar123 <106126696+dasalgar123@users.noreply.github.com>
2024-10-08 16:19:18 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+class ButtonHandler {
+    constructor() {
+        this.initEventListeners();
+    }
+
+    // Método que inicializa los eventos
+    initEventListeners() {
+        document.getElementById("btnIniciarSesion").addEventListener("click", () => {
+            this.redirectToPage("https://juegentrar.netlify.app"); // Ajusta la ruta según sea necesario
+        });
+
+        document.getElementById("btnCrearUsuario").addEventListener("click", () => {
+            this.redirectToPage("./crear_usuario.html");
+        });
+
+        document.getElementById("btnOlvidoContrasena").addEventListener("click", () => {
+            this.redirectToPage("./olvido_contrasena.html");
+        });
+    }
+
+    // Método para redirigir a una página
+    redirectToPage(pageUrl) {
+        window.location.href = pageUrl;
+    }
+}
+
+// Crear una instancia de la clase ButtonHandler cuando el DOM esté cargado
+document.addEventListener("DOMContentLoaded", () => {
+    new ButtonHandler();
+});
