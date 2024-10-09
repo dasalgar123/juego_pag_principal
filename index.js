571fe03992f0dc363d24266eca567085687e30f7
dasalgar123 <106126696+dasalgar123@users.noreply.github.com>
2024-10-09 01:07:32 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Piedra, Papel, Tijeras - Inicio</title>
-    <link rel="stylesheet" href="index.css">
-</head>
-<body>
-    <header>
-        <div id="app">
-            <div class="frame"><h1 id="titulo_principal">Juegos Sanos</h1></div>
-        </div>
-    </header>    
-    <section class="contenido_formulario">
-        <form action="">
-            <button id="btnIniciarSesion" type="button" >Iniciar Sesión</button>
-            <button id="btnCrearUsuario" type="button">Crear Usuario</button>
-            <button id="btnOlvidoContrasena" type="button">juegos</button>
-        </form>
-    </section>
-    
-    </div>
-            <div><img id="fondo_principal" src="fondo_2.jpg" alt=""></div>
-    </div>
+class ButtonHandler {
+    constructor() {
+        this.initEventListeners();
+    }
 
+    // Método que inicializa los eventos
+    initEventListeners() {
+        document.getElementById("btnIniciarSesion").addEventListener("click", () => {
+            this.redirectToPage("https://juegoinicio.netlify.app"); // Ajusta la ruta según sea necesario
+        });
+
+        document.getElementById("btnCrearUsuario").addEventListener("click", () => {
+            this.redirectToPage("https://juegoregistro.netlify.app");
+        });
+
+        document.getElementById("btnOlvidoContrasena").addEventListener("click", () => {
+            this.redirectToPage("./olvido_contrasena.html");
+        });
+    }
+
+    // Método para redirigir a una página
+    redirectToPage(pageUrl) {
+        window.location.href = pageUrl;
+    }
+}
 
 // Crear una instancia de la clase ButtonHandler cuando el DOM esté cargado
 document.addEventListener("DOMContentLoaded", () => {
